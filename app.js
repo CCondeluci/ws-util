e4a336caaff2850247877dff41bc54cb6f8f5ee9
CCondeluci <crc73@pitt.edu>
2019-12-13 08:50:40 -0500

    Added basic HTML page for ref cards service.

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const serveStatic = require('serve-static');
+const path = require('path');
 
 const app = express();
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(serveStatic(path.join(__dirname, 'views')));
 
 let routes = require('./routes');
 app.use(routes);
