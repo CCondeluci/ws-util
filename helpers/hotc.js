842d76175921aba0f590b58443a3638283d0b7da
Carmen Condeluci <crc73@pitt.edu>
2024-09-21 10:07:45 -0400

    more logging

--- helpers/hotc.js
+++ helpers/hotc.js
@@ -16,20 +16,24 @@ process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
  */
 
 module.exports.getRefCard = async function (cardCode, fileType) {
-    var options = {
-        url: HOTC.CARD_LIST_URL + cardCode + HOTC.SHORT_QUERY,
-        json: true
-    };
-    let body = await request(options);
-    // parse and index full html response
-    let root = cheerio.load(body);
-    // get img name and filetype
-    let cardImg = root('img').attr('src');
-    cardImg = cardImg.split('heartofthecards/images/cards/ws/')[1];
-    // set the img link to true hotc
-    root('img').first().attr('src', HOTC.IMG_URL + cardImg); //cardCode.replace('/', '-').toLowerCase() + '.gif');
-    // recreate ref card from response
-    let refCard = '<table width="400" style="border:1px solid black">' + root('table').html() + '</table>';
-
-    return refCard;
+    try {
+        var options = {
+            url: HOTC.CARD_LIST_URL + cardCode + HOTC.SHORT_QUERY,
+            json: true
+        };
+        let body = await request(options);
+        // parse and index full html response
+        let root = cheerio.load(body);
+        // get img name and filetype
+        let cardImg = root('img').attr('src');
+        cardImg = cardImg.split('heartofthecards/images/cards/ws/')[1];
+        // set the img link to true hotc
+        root('img').first().attr('src', HOTC.IMG_URL + cardImg); //cardCode.replace('/', '-').toLowerCase() + '.gif');
+        // recreate ref card from response
+        let refCard = '<table width="400" style="border:1px solid black">' + root('table').html() + '</table>';
+    
+        return refCard;
+    } catch (error) {
+        console.log("REFCARD ERROR", cardCode, error);
+    }
 }
